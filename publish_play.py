e7cb76dfcdd91593d3769bae8342495d62f70d0d
USAMAWIZARD <usamawizard@gmail.com>
2025-02-12 17:19:48 +0530

    add option for commandline argument

--- publish_play.py
+++ publish_play.py
@@ -1,10 +1,18 @@
+import argparse
 from webrtc import WebRTCAdapter, init_gstreamer
 
-appname = "live"
-WEBSOCKET_URL = 'wss://test.antmedia.io/' + appname + '/websocket'
+parser = argparse.ArgumentParser(description="WebRTC Stream Player/Publisher")
+parser.add_argument("websocket_url", help="Full WebSocket URL (e.g., ws://ip:port/appname/websocket)")
+parser.add_argument("stream_id", help="Stream ID to play or publish")
+parser.add_argument("mode", choices=["play", "publish"], help="Mode: 'play' to play a stream, 'publish' to publish a stream")
+
+args = parser.parse_args()
 
 init_gstreamer()
-webrtc_adapter = WebRTCAdapter(WEBSOCKET_URL)
+webrtc_adapter = WebRTCAdapter(args.websocket_url)
 webrtc_adapter.connect()
-# replace publish call with play to play the stream
-webrtc_adapter.play("test1")
+
+if args.mode == "play":
+    webrtc_adapter.play(args.stream_id)
+elif args.mode == "publish":
+    webrtc_adapter.publish(args.stream_id)
